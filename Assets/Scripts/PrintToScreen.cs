adc751517f29183e17c9cbc0e98674433d000c18
ChinoBeach <beach988@morrisville.edu>
2022-08-31 11:02:02 -0400

    Your First Script

---
+++ Assets/Scripts/PrintToScreen.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintToScreen : MonoBehaviour
+{
+    void Start()
+    {
+        //print to console "Hellow World"
+        Debug.Log("Hello World");
+    }
+
+    void Update()
+    {
+        
+    }
+}
