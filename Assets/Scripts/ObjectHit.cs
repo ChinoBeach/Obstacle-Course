8339e88da0d50ed860040b88f70976f59937794b
ChinoBeach <beach988@morrisville.edu>
2022-09-05 12:51:10 -0400

    Using GetComponent<>()

--- Assets/Scripts/ObjectHit.cs
+++ Assets/Scripts/ObjectHit.cs
@@ -7,7 +7,14 @@ public class ObjectHit : MonoBehaviour
     //when something collides with the object this script is attached to
     private void OnCollisionEnter(Collision other)
     {
+        
+
+        //tell the consol that a wall was hit
         Debug.Log("Bumped into a wall");
+
+        //change the color of the wall
+        GetComponent<MeshRenderer>().material.color = Color.gray;
+
     }
 
 
