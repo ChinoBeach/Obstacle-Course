25ff9b7629910a8ba07a6a7f1700a33344188b75
ChinoBeach <beach988@morrisville.edu>
2022-09-11 13:48:44 -0400

    Using Tags

--- Assets/Scripts/ObjectHit.cs
+++ Assets/Scripts/ObjectHit.cs
@@ -12,8 +12,17 @@ public class ObjectHit : MonoBehaviour
         //tell the consol that a wall was hit
         //Debug.Log("Bumped into a wall");
 
-        //change the color of the wall
-        GetComponent<MeshRenderer>().material.color = Color.gray;
+        //IF the PLAYER hits something
+
+        if(other.gameObject.tag == "Player")
+        {
+            //change the color of the wall
+            GetComponent<MeshRenderer>().material.color = Color.gray;
+
+            //Tag the object as being hit
+            gameObject.tag = "Hit";
+        }
+        
 
     }
 
--- Assets/Scripts/Scorer.cs
+++ Assets/Scripts/Scorer.cs
@@ -10,11 +10,16 @@ public class Scorer : MonoBehaviour
     //If something hits the player or the player hits something
     private void OnCollisionEnter(Collision other)
     {
-        //incremenet number of times hit something
-        int_numTimesHit++;
-        
-        //Tell the consol how many times you have hit something
-        Debug.Log("You've bumped into a thig this many times: " + int_numTimesHit);
+        // if you have not already hit this object
+        if(other.gameObject.tag != "Hit")
+        {
+            //incremenet number of times hit something
+            int_numTimesHit++;
+
+            //Tell the consol how many times you have hit something
+            Debug.Log("You've bumped into a thig this many times: " + int_numTimesHit);
+        }
+       
         
     }
 }
